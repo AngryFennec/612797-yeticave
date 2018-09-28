949d11f5d937dc325efe56ef82f0756d154ca1d5
Angry Fennec <angryfennecus@gmail.com>
2018-09-28 07:50:28 +0300

    перенесла функцию

--- functions.php
+++ functions.php
@@ -15,4 +15,14 @@ function render_template($name, $data) {
 
     return $result;
 }
+
+
+function format_sum($sum) {
+    $round_sum = ceil($sum);
+    $sum_string = ceil($round_sum) . "₽";
+    if ($round_sum >= 1000) {
+        $sum_string = number_format(ceil($sum), 0, ".", " ") . "₽";
+    }
+    return $sum_string;
+}
 ?>
