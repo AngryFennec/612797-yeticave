51471eb239eaa40efe0203660c6bc9eb19ff6573
Fennec <shellfix@gmail.com>
2018-10-11 18:33:39 +0300

    валидация

--- templates/add.php
+++ templates/add.php
@@ -1,12 +1,13 @@
-<form class="form form--add-lot container form--invalid" action="add.php" method="post"> <!-- form--invalid -->
+<?php var_dump($errors);?>
+<form class="form form--add-lot container <?php if(!empty($errors)) echo "form--invalid";?>" action="add.php" method="post"> <!-- form--invalid -->
   <h2>Добавление лота</h2>
   <div class="form__container-two">
-    <div class="form__item form__item--invalid"> <!-- form__item--invalid -->
+    <div class="form__item <?php isset($errors['lot-name']) ? "form__item--invalid" : ""?>"> <!-- form__item--invalid -->
       <label for="lot-name">Наименование</label>
       <input id="lot-name" type="text" name="lot-name" placeholder="Введите наименование лота" required>
-      <span class="form__error">Введите наименование лота</span>
+      <span class="form__error"><?=$errors['lot-name'];?></span>
     </div>
-    <div class="form__item">
+    <div class="form__item  <?php echo isset($errors['category']) ? "form__item--invalid" : "";?>">
       <label for="category">Категория</label>
       <select id="category" name="category" required>
         <?php foreach ($categories as $key => $val):?>
@@ -19,38 +20,32 @@
   <div class="form__item form__item--wide">
     <label for="message">Описание</label>
     <textarea id="message" name="message" placeholder="Напишите описание лота" required></textarea>
-    <span class="form__error">Напишите описание лота</span>
+    <span class="form__error"><?=$errors['message'];?></span>
   </div>
   <div class="form__item form__item--file"> <!-- form__item--uploaded -->
     <label>Изображение</label>
-    <div class="preview">
-      <button class="preview__remove" type="button">x</button>
-      <div class="preview__img">
-        <img src="img/avatar.jpg" width="113" height="113" alt="Изображение лота">
-      </div>
-    </div>
     <div class="form__input-file">
-      <input class="visually-hidden" type="file" id="photo2" value="" name="photo2">
-      <label for="photo2">
+      <input class="visually-hidden" type="file" id="photo" value="" name="photo">
+      <label for="photo">
         <span>+ Добавить</span>
       </label>
     </div>
   </div>
   <div class="form__container-three">
-    <div class="form__item form__item--small">
+    <div class="form__item form__item--small <?php echo isset($errors['lot-rate']) ? "form__item--invalid" : "";?>">
       <label for="lot-rate">Начальная цена</label>
       <input id="lot-rate" type="number" name="lot-rate" placeholder="0" required>
-      <span class="form__error">Введите начальную цену</span>
+      <span class="form__error"><?=$errors['lot-rate'];?></span>
     </div>
-    <div class="form__item form__item--small">
+    <div class="form__item form__item--small  <?php echo isset($errors['lot-step']) ? "form__item--invalid" : "";?>">
       <label for="lot-step">Шаг ставки</label>
       <input id="lot-step" type="number" name="lot-step" placeholder="0" required>
-      <span class="form__error">Введите шаг ставки</span>
+      <span class="form__error"><?=$errors['lot-step'];?></span>
     </div>
-    <div class="form__item">
+    <div class="form__item <?php echo isset($errors['lot-date']) ? "form__item--invalid" : "";?>">
       <label for="lot-date">Дата окончания торгов</label>
       <input class="form__input-date" id="lot-date" type="date" name="lot-date" required>
-      <span class="form__error">Введите дату завершения торгов</span>
+      <span class="form__error"><?=$errors['lot-date'];?></span>
     </div>
   </div>
   <span class="form__error form__error--bottom">Пожалуйста, исправьте ошибки в форме.</span>
