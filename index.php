7540e14331b584f52bc183a8162ef58cf0d92321
AngryFennec <shellfix@gmail.com>
2018-10-16 17:40:23 +0300

    лоты по категории

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
 require_once('init.php');
 
-$sql_lot = "SELECT lot_id, name, c.cat_name, l.category_id, sum, img, end_date FROM lots l JOIN categories c ON l.category_id = c.category_id WHERE l.end_date > NOW() ORDER BY l.init_date DESC LIMIT 6";
+if (isset($_GET['category_id'])) {
+  $cat_id = intval($_GET['category_id']);
+  $sql_lot = "SELECT lot_id, name, c.cat_name, l.category_id, sum, img, end_date FROM lots l JOIN categories c ON l.category_id = ". $cat_id . " WHERE l.end_date > NOW() ORDER BY l.init_date DESC LIMIT 6";
+} else {
+  $sql_lot = "SELECT lot_id, name, c.cat_name, l.category_id, sum, img, end_date FROM lots l JOIN categories c ON l.category_id = c.category_id WHERE l.end_date > NOW() ORDER BY l.init_date DESC LIMIT 6";
+}
 $result_lot = mysqli_query($con, $sql_lot);
 if ($result_lot) {
     $lots = mysqli_fetch_all($result_lot, MYSQLI_ASSOC);
 }
 
-$page_content = render_template('index.php', ['categories' => $categories, 'lots' => $lots]);
+$page_content = render_template('index.php', ['categories' => $categories, 'lots' => $lots, 'cat_id' => $cat_id]);
 $layout_content = render_template('layout.php', ['page_content' => $page_content, 'title' => 'Главная', 'categories' => $categories,  'user' => $user]);
 print($layout_content);
 
