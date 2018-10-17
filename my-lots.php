366508b05b4da3d246464f94aedd790314cde050
Angry Fennec <angryfennecus@gmail.com>
2018-10-17 20:01:24 +0300

    мои ставки

--- my-lots.php
+++ my-lots.php
@@ -6,14 +6,13 @@ if (empty($user)) {
     exit();
 }
 $my_bets = [];
-$sql_bet = "SELECT b.lot_id, b.user_id, l.winner_id, u.contacts, l.name, l.category_id, l.end_date, l.img, b.sum, b.init_date FROM bets b JOIN lots l ON l.lot_id = b.lot_id JOIN users u ON b.user_id = u.user_id WHERE b.user_id = " . $user['user_id'];
+$sql_bet = "SELECT b.lot_id, b.user_id, l.winner_id, u.contacts, l.name, l.category_id, c.cat_name, l.end_date, l.img, b.sum, b.init_date FROM bets b JOIN lots l ON l.lot_id = b.lot_id JOIN users u ON b.user_id = u.user_id JOIN categories c ON l.category_id = c.category_id WHERE b.user_id = " . $user['user_id'];
 $result_bet = mysqli_query($con, $sql_bet);
 if ($result_bet) {
     $my_bets = mysqli_fetch_all($result_bet, MYSQLI_ASSOC);
 }
 
-var_dump($my_bets);
-$page_content = render_template('my-lost.php', ['categories' => $categories, 'my_bets' => $my_bets]);
+$page_content = render_template('my-lots.php', ['categories' => $categories, 'my_bets' => $my_bets]);
 $layout_content = render_template('layout.php', ['page_content' => $page_content, 'title' => 'Мои ставки', 'categories' => $categories,  'user' => $user]);
 print($layout_content);
 ?>
--- templates/my-lots.php
+++ templates/my-lots.php
@@ -1,3 +1,13 @@
+<nav class="nav">
+    <ul class="nav__list container">
+        <!--заполните этот список из массива категорий-->
+        <?php foreach($categories as $value):?>
+            <li class="nav__item">
+                <a href="/?category=" . <?=$value['category_id']?>><?=$value['cat_name'] ?></a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</nav>
 <section class="rates container">
   <h2>Мои ставки</h2>
   <table class="rates__list">
@@ -5,7 +15,7 @@
       <tr class="rates__item <?=!empty($val['winner_id']) ? ' rates__item--win' : ''?> <?=strtotime($val['end_date']) > time() ? ' rates__item--end' : ''?>">
       <td class="rates__info">
         <div class="rates__img">
-          <img src="<?=!empty($val['img']) ? $val['img'] : ''?>" width="54" height="40" alt="<?=!empty($val['category_id']) ? $categories[$val['category_id']] : ''?>">
+          <img src="<?=!empty($val['img']) ? $val['img'] : ''?>" width="54" height="40" alt="<?=!empty($val['cat_name']) ? $val['cat_name'] : ''?>">
         </div>
         <h3 class="rates__title"><a href="lot.php?lot_id=<?=$val['lot_id']?>"><?=$val['name']?></a></h3>
         <?php if(!empty($val['winner_id'])):?>
@@ -13,12 +23,12 @@
       <? endif ?>
       </td>
       <td class="rates__category">
-        <?=$categories[$val['category_id']]?>
+        <?php $val['cat_name']?>
       </td>
       <td class="rates__timer">
         <?php if(!empty($val['winner_id'])): ?>
         <div class="timer timer--win">Ставка выиграла</div>
-        <?php elseif (strtotime($val['end_date']) > time()): ?>
+    <?php elseif (strtotime($val['end_date']) < time()): ?>
           <div class="timer timer--end">Торги окончены</div>
         <?php else: ?>
         <div class="timer timer--finishing"><?=get_formatted_time($val['end_date']);?></div>
