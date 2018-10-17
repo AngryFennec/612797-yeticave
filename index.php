44cb220af3e99073f2d2a7e592307c1364b9a5b5
Angry Fennec <angryfennecus@gmail.com>
2018-10-17 07:59:09 +0300

    Теперь точно по категориям

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 require_once('init.php');
 
-if (isset($_GET['category_id'])) {
-  $cat_id = intval($_GET['category_id']);
-  $sql_lot = "SELECT lot_id, name, c.cat_name, l.category_id, sum, img, end_date FROM lots l JOIN categories c ON l.category_id = ". $cat_id . " WHERE l.end_date > NOW() ORDER BY l.init_date DESC LIMIT 9";
+$cat_id = "";
+if (isset($_GET['category'])) {
+  $cat_id = intval($_GET['category']);
+  $sql_lot = "SELECT lot_id, name, c.cat_name, l.category_id, sum, img, end_date FROM lots l JOIN categories c ON l.category_id = c.category_id WHERE l.end_date > NOW() AND l.category_id = ". $cat_id . " ORDER BY l.init_date DESC LIMIT 9";
 } else {
   $sql_lot = "SELECT lot_id, name, c.cat_name, l.category_id, sum, img, end_date FROM lots l JOIN categories c ON l.category_id = c.category_id WHERE l.end_date > NOW() ORDER BY l.init_date DESC LIMIT 6";
 }
+print($cat_id);
 $result_lot = mysqli_query($con, $sql_lot);
 if ($result_lot) {
     $lots = mysqli_fetch_all($result_lot, MYSQLI_ASSOC);
